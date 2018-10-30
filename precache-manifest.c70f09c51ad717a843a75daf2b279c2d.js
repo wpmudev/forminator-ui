cf33d1e37ac24d2bdd323d72cb99f4e79416447f
Leighton Sapir <leigh@incsub.com>
2018-10-30 08:54:50 -0500

    Updates

---
+++ precache-manifest.c70f09c51ad717a843a75daf2b279c2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/forminator-ui/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f5a5ffff4cec71add6a6",
+    "url": "/forminator-ui/static/js/runtime~main.f5a5ffff.js"
+  },
+  {
+    "revision": "284c7796d41f07c0da1b",
+    "url": "/forminator-ui/static/js/main.284c7796.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/forminator-ui/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "284c7796d41f07c0da1b",
+    "url": "/forminator-ui/static/css/main.f8c27686.chunk.css"
+  },
+  {
+    "revision": "53e3f1be644745ff80daa147d6de6b36",
+    "url": "/forminator-ui/index.html"
+  }
+];
