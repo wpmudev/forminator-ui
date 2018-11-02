f86efd84de5d7ae295090925f8eb770183c0d411
Leighton Sapir <leigh@incsub.com>
2018-11-02 12:07:45 -0500

    [update] get new container

--- src/form-ui/page.js
+++ src/form-ui/page.js
@@ -4,8 +4,7 @@ import Header from '../containers/header';
 import Tabs from '../components/tabs';
 
 import Grid from './containers/grid';
-import InputSample from './containers/input';
-import Themes from './containers/themes';
+import Elements from './containers/elements';
 import Sample from './containers/sample';
 
 import './themes/default.css';
@@ -23,10 +22,8 @@ export default class PageForm extends Component {
 					<Tabs default="grid">
 						<Grid label="Grid"
 							value="grid" />
-						<InputSample label="Input"
-							value="input" />
-						<Themes label="Themes"
-							value="themes" />
+						<Elements label="Elements"
+							value="elements" />
 						<Sample label="Sample Form"
 							value="sample" />
 					</Tabs>
