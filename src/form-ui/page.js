1bcd978719457bd874d74c41788c89d0a97e110e
Leighton Sapir <leigh@incsub.com>
2018-11-02 00:06:35 -0500

    [update] get form themes

--- src/form-ui/page.js
+++ src/form-ui/page.js
@@ -4,9 +4,14 @@ import Header from '../containers/header';
 import Tabs from '../components/tabs';
 
 import Grid from './containers/grid';
-import Elements from './containers/elements';
+import InputSample from './containers/input';
 import Themes from './containers/themes';
 import Sample from './containers/sample';
+
+import './themes/default.css';
+import './themes/flat.css';
+import './themes/bold.css';
+import './themes/material.css';
 
 export default class PageForm extends Component {
 	render() {
@@ -18,8 +23,8 @@ export default class PageForm extends Component {
 					<Tabs default="grid">
 						<Grid label="Grid"
 							value="grid" />
-						<Elements label="Elements"
-							value="elements" />
+						<InputSample label="Input"
+							value="input" />
 						<Themes label="Themes"
 							value="themes" />
 						<Sample label="Sample Form"
