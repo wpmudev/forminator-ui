752a0a655b70c76b610e11e3d43c97aabd9247db
Leighton Sapir <leigh@incsub.com>
2018-11-02 15:27:16 -0500

    [update] elements: get input and textarea samples

--- src/form-ui/containers/elements.js
+++ src/form-ui/containers/elements.js
@@ -1,120 +1,15 @@
 import React, { Component } from 'react';
 
-import Options from '../../components/options';
-
-import Form from '../components/form';
-import Row from '../components/row';
-import Input from '../components/input';
+import InputSample from '../samples/input';
+import TextareaSample from '../samples/textarea';
 
 export default class Elements extends Component {
 	render() {
 		return (
 			<React.Fragment>
 
-				<h2>Input Field</h2>
-
-				<p>This field can be used to enter regular text, for example when asking for a company's name.</p>
-
-				<Options default="default">
-
-					<Form design="default"
-						value="default"
-						label="Default">
-						<Row>
-							<Input property="input-default"
-								label="Field label"
-								placeholder="Placeholder"
-								description="Description for input field." />
-						</Row>
-						<Row>
-							<Input property="input-required-default"
-								label="Required field"
-								placeholder="Placeholder"
-								description="Description for input field."
-								isRequired
-								hasError />
-						</Row>
-					</Form>
-
-					<Form design="flat"
-						value="flat"
-						label="Flat">
-						<Row>
-							<Input property="input-flat"
-								label="Field label"
-								placeholder="Placeholder"
-								description="Description for input field." />
-						</Row>
-						<Row>
-							<Input property="input-required-flat"
-								label="Required field"
-								placeholder="Placeholder"
-								description="Description for input field."
-								isRequired
-								hasError />
-						</Row>
-					</Form>
-
-					<Form design="bold"
-						value="bold"
-						label="Bold">
-						<Row>
-							<Input property="input-bold"
-								label="Field label"
-								placeholder="Placeholder"
-								description="Description for input field." />
-						</Row>
-						<Row>
-							<Input property="input-required-bold"
-								label="Required field"
-								placeholder="Placeholder"
-								description="Description for input field."
-								isRequired
-								hasError />
-						</Row>
-					</Form>
-
-					<Form design="material"
-						value="material"
-						label="Material">
-						<Row>
-							<Input property="input-material"
-								label="Field label"
-								placeholder="Placeholder"
-								description="Description for input field."
-								isMaterial />
-						</Row>
-						<Row>
-							<Input property="input-required-material"
-								label="Required field"
-								placeholder="Placeholder"
-								description="Description for input field."
-								isMaterial
-								isRequired
-								hasError />
-						</Row>
-					</Form>
-
-					<Form design="none"
-						value="none"
-						label="None">
-						<Row>
-							<Input property="input-none"
-								label="Field label"
-								placeholder="Placeholder"
-								description="Description for input field." />
-						</Row>
-						<Row>
-							<Input property="input-required-none"
-								label="Required field"
-								placeholder="Placeholder"
-								description="Description for input field."
-								isRequired
-								hasError />
-						</Row>
-					</Form>
-
-				</Options>
+				<InputSample />
+				<TextareaSample />
 
 			</React.Fragment>
 		);
