5e87c03735e196aed664294278eae835249552ef
Leighton Sapir <leigh@incsub.com>
2018-11-02 20:48:58 -0500

    [update] [component] input

--- src/form-ui/samples/input.js
+++ src/form-ui/samples/input.js
@@ -6,19 +6,53 @@ import Input from '../components/input';
 
 export default class InputSample extends Component {
 	render() {
+		const design = this.props.design
+			? this.props.design
+			: 'default'
+			;
+
+		let label = '';
+
+		if ( this.props.label ) {
+			label = (
+				<span className="block-label dark">
+					{ this.props.label }
+				</span>
+			);
+		}
+
+		let description = '';
+
+		if ( this.props.description ) {
+			description = (
+				<span className="description">
+					{ this.props.description }
+				</span>
+			);
+		}
+
+		let blockSide = '';
+
+		if ( this.props.label || this.props.description ) {
+
+			blockSide = (
+				<div className="block-side">
+					{ label }
+					{ description }
+				</div>
+			);
+		}
+
 		return (
 			<React.Fragment>
 
 				<div className="block">
 
-					<div className="block-side">
-						<span className="block-label dark">Input</span>
-						<span className="description">This element style is used for several form fields, like: input, name, email, number, etc.</span>
-					</div>
+					{ blockSide }
 
 					<div className="block-content">
 
-						<Form design="default">
+						<Form design={ design }>
 							<Row>
 								<Input property="input-default"
 									label="Field label"
