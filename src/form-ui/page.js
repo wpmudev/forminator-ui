c6868cb41b904020c48d666b1f3cfcc45a2476a0
Leighton Sapir <leigh@incsub.com>
2018-11-02 23:22:23 -0500

    [update] get new containers for form ui page

--- src/form-ui/page.js
+++ src/form-ui/page.js
@@ -4,8 +4,10 @@ import Header from '../containers/header';
 import Tabs from '../components/tabs';
 
 import Grid from './containers/grid';
-import Elements from './containers/elements';
-import Sample from './containers/sample';
+import Default from './containers/default';
+import Flat from './containers/flat';
+import Bold from './containers/bold';
+import Material from './containers/material';
 
 import './themes/default.css';
 import './themes/flat.css';
@@ -22,10 +24,14 @@ export default class PageForm extends Component {
 					<Tabs default="grid">
 						<Grid label="Grid"
 							value="grid" />
-						<Elements label="Elements"
-							value="elements" />
-						<Sample label="Sample Form"
-							value="sample" />
+						<Default label="Default"
+							value="default" />
+						<Flat label="Flat"
+							value="flat" />
+						<Bold label="Bold"
+							value="bold" />
+						<Material label="Material"
+							value="material" />
 					</Tabs>
 				</section>
 			</React.Fragment>
