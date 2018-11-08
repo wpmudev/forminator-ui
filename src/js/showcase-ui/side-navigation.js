788a8ecda34e84f062bb02dea8ad1e1247dea332
Leighton Sapir <leigh@incsub.com>
2018-11-08 11:41:53 -0500

    [update] load global nav with js

--- src/js/showcase-ui/side-navigation.js
+++ src/js/showcase-ui/side-navigation.js
@@ -1,5 +1,31 @@
 ( function( $ ) {
 
+	// Load navigation
+	$( 'body' ).ready( function() {
+
+		var $navigation = $( '.navigation-main' );
+
+		var $button = '<button class="navigation-button">' +
+			'<i aria-hidden="true"></i>' +
+			'<i aria-hidden="true"></i>' +
+			'<i aria-hidden="true"></i>' +
+			'<span>Menu</span>' +
+		'</button';
+
+		var $title = '<span class="navigation-title">Forminator UI</span>';
+
+		var $sidenav = '<ul class="navigation-side">' +
+			'<li><a href="/">Home</a></li>' +
+			'<li><a href="form-ui.html">Forms UI</a></li>' +
+		'</ul>';
+
+		$navigation.append( $button );
+		$navigation.append( $title );
+		$navigation.append( $sidenav );
+
+	});
+
+	// Open close navigation
 	$( 'body' ).on( 'click', '.navigation-button', function() {
 
 		var showcase = $( this ).closest( '.showcase' );
