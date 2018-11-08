5afd2d916dc8bd0ac6acf939c9e846e694cd6c9f
Leighton Sapir <leigh@incsub.com>
2018-11-08 18:22:20 -0500

    [update] showcase: append sample fields

--- src/js/showcase-ui/form-samples.js
+++ src/js/showcase-ui/form-samples.js
@@ -4,41 +4,35 @@
 
 		$( '.forminator-custom-form' ).each( function() {
 
-			var $form = $( this ),
-				$isValid = ( true === $form.data( 'sample' ) ),
-				$isInput = ( 'input' === $form.data( 'element' ) )
+			var $form       = $( this ),
+				$isValid    = ( true === $form.data( 'sample' ) ),
+				$isInput    = ( 'input' === $form.data( 'element' ) ),
+				$isTextarea = ( 'textarea' === $form.data( 'element' ) )
 				;
 
-			var $sampleInput   = $( '.forminator-custom-form[data-element="input"]' ),
-				$simpleInput   = '<div class="forminator-row">' +
-					'<div class="forminator-col">' +
-						'<div class="forminator-field">' +
-							'<label class="forminator-label">Field label</label>' +
-							'<input placeholder="Placeholder"' +
-								'size="1"' +
-								'class="forminator-input" />' +
-							'<span class="forminator-description">Description for input field.</span>' +
-						'</div>' +
-					'</div>' +
-				'</div>',
-				$requiredInput = '<div class="forminator-row">' +
-					'<div class="forminator-col">' +
-						'<div class="forminator-field">' +
-							'<label class="forminator-label">Required field <span class="forminator-required">*</span></label>' +
-							'<input placeholder="Placeholder"' +
-								'size="1"' +
-								'class="forminator-input" />' +
-							'<span className="forminator-error-message">This field is required.</span>' +
-							'<span class="forminator-description">Description for input field.</span>' +
-						'</div>' +
-					'</div>' +
-				'</div>'
+			var $tplInput    = $( '#forminator-form-element-tpl--input' ).html(),
+				$tplInputReq = $( '#forminator-form-element-tpl--input-req' ).html()
 				;
+
+			var $tplTextarea    = $( '#forminator-form-element-tpl--textarea' ).html(),
+				$tplTextareaReq = $( '#forminator-form-element-tpl--textarea-req' ).html()
+				;
+
+			// Get form design
+			if ( typeof $form.data( 'design' ) !== undefined ) {
+				$form.addClass( 'forminator-design--' + $form.data( 'design' ) );
+			}
 
 			// SAMPLE: Input field
 			if ( $isValid && $isInput ) {
-				$sampleInput.append( $simpleInput );
-				$sampleInput.append( $requiredInput );
+				$form.append( $tplInput );
+				$form.append( $tplInputReq );
+			}
+
+			// SAMPLE: Textarea field
+			if ( $isValid && $isTextarea ) {
+				$form.append( $tplTextarea );
+				$form.append( $tplTextareaReq );
 			}
 		});
 
