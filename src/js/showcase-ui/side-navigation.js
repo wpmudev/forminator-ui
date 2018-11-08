f15cb31156402e88371586808e69ca3e6613b2c4
Leighton Sapir <leigh@incsub.com>
2018-11-08 01:59:18 -0500

    [update] showcase: side navigation

--- src/js/showcase-ui/side-navigation.js
+++ src/js/showcase-ui/side-navigation.js
@@ -1,7 +1,14 @@
 ( function( $ ) {
 
-	$( 'body' ).on( 'load', function() {
-		console.log( 'TEST' );
+	$( 'body' ).on( 'click', '.navigation-button', function() {
+
+		var showcase = $( this ).closest( '.showcase' );
+
+		if ( showcase.hasClass( 'is-open' ) ) {
+			showcase.removeClass( 'is-open' );
+		} else {
+			showcase.addClass( 'is-open' );
+		}
 	});
 
 }( jQuery ) );
