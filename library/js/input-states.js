61ebd14c219d9a5f3c6d0fb020b398875a172289
Leighton Sapir <leigh@incsub.com>
2019-02-06 12:37:23 -0500

    [new] input states js function

---
+++ library/js/input-states.js
@@ -0,0 +1,87 @@
+( function( $ ) {
+
+	// Enable strict mode.
+	'use strict';
+
+	// Define global SUI object if it doesn't exist.
+	if ( 'object' !== typeof window.FUI ) {
+		window.FUI = {};
+	}
+
+	FUI.inputStates = function( el ) {
+
+		const input = $( el );
+
+		if ( ! input.is( 'input' ) ) {
+			return;
+		}
+
+		function hover( element ) {
+
+			const getInput = $( element );
+			const getField = getInput.closest( '.forminator-field' );
+
+			getInput.mouseover( function( e ) {
+				getField.addClass( 'forminator-is_hover' );
+				e.stopPropagation();
+			}).mouseout( function( e ) {
+				getField.removeClass( 'forminator-is_hover' );
+				e.stopPropagation();
+			});
+		}
+
+		function focused( element ) {
+
+			const getInput = $( element );
+			const getField = getInput.closest( '.forminator-field' );
+
+			getInput.focus( function( e ) {
+				getField.addClass( 'forminator-is_active' );
+				e.stopPropagation();
+			}).blur( function( e ) {
+				getField.removeClass( 'forminator-is_active' );
+				e.stopPropagation();
+			});
+		}
+
+		function filled( element ) {
+
+			const getInput = $( element );
+			const getField = getInput.closest( '.forminator-field' );
+
+			// On input load
+			getInput.on( 'load', function() {
+
+				if ( '' !== getInput.val().trim() ) {
+					getField.addClass( 'forminator-is_filled' );
+				}
+			});
+
+			// On input changes
+			getInput.on( 'change', function() {
+
+				if ( '' !== getInput.val().trim() ) {
+					getField.addClass( 'forminator-is_filled' );
+				} else {
+					getField.removeClass( 'forminator-is_filled' );
+				}
+			});
+		}
+
+		function init() {
+
+			input.each( function() {
+
+				hover( this );
+				focused( this );
+				filled( this );
+
+			});
+		}
+
+		init();
+
+		return this;
+	};
+
+}( jQuery ) );
