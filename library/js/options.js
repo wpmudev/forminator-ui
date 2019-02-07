6f81e3bf4acfa148e1eba8aab8f4adeee9f17716
Leighton Sapir <leigh@incsub.com>
2019-02-06 23:02:22 -0500

    [update] multi select states

--- library/js/options.js
+++ library/js/options.js
@@ -81,4 +81,37 @@
 		return this;
 	};
 
+	FUI.multiSelectStates = function( el ) {
+
+		const container = $( el );
+		const option = container.find( '.forminator-option' );
+		const input = option.find( 'input' );
+
+		if ( ! container.is( '.forminator-multiselect' ) || 0 === option.length ) {
+			return;
+		}
+
+		function init() {
+
+			input.each( function() {
+
+				$( this ).on( 'click', function() {
+
+					const checkInput = $( this );
+					const checkLabel = checkInput.parent();
+
+					if ( checkLabel.is( '.forminator-is_checked' ) ) {
+						checkLabel.removeClass( 'forminator-is_checked' );
+					} else {
+						checkLabel.addClass( 'forminator-is_checked' );
+					}
+				});
+			});
+		}
+
+		init();
+
+		return this;
+	};
+
 }( jQuery ) );
