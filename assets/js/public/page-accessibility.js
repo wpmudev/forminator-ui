04e9adffdd990859252ccd88971f6bdfd6ddc2a8
Leighton Sapir <leigh@incsub.com>
2019-02-06 20:35:08 -0500

    [update] get required settings

--- assets/js/public/page-accessibility.js
+++ assets/js/public/page-accessibility.js
@@ -1,5 +1,103 @@
 ( function( $ ) {
 
+	'use strict';
+
+	// Define global SHOWCASE object if it doesn't exist.
+	if ( 'object' !== typeof window.SHOWCASE ) {
+		window.SHOWCASE = {};
+	}
+
+	SHOWCASE.requiredSettings = function( el ) {
+
+		const column = $( el );
+		const required = '<span class="forminator-required">*</span>';
+
+		if ( ! column.is( '.forminator-col' ) ) {
+			return;
+		}
+
+		function requiredInput( col ) {
+
+			const column = $( col );
+			const input = column.find( '.forminator-input' );
+
+			input.each( function() {
+
+				const field = $( this ).closest( '.forminator-field' );
+				const label = field.find( '.forminator-label' );
+				const text = label.text();
+
+				$( this ).attr( 'aria-required', 'true' );
+
+				if ( label.length ) {
+					label.html( text + ' ' + required );
+				}
+
+				field.addClass( 'forminator-is_required' );
+			});
+
+		}
+
+		function requiredTextarea( col ) {
+
+			const column = $( col );
+			const textarea = column.find( '.forminator-textarea' );
+
+			textarea.each( function() {
+
+				const field = $( this ).closest( '.forminator-field' );
+				const label = field.find( '.forminator-label' );
+				const text = label.text();
+
+				$( this ).attr( 'aria-required', 'true' );
+
+				if ( label.length ) {
+					label.html( text + ' ' + required );
+				}
+
+				field.addClass( 'forminator-is_required' );
+			});
+
+		}
+
+		function requiredRadio( col ) {
+
+			const column = $( col );
+			const field = column.find( '.forminator-field' );
+			const radio = column.find( '.forminator-radio' );
+			const label = column.find( '.forminator-label' );
+			const text = label.text();
+
+			radio.each( function() {
+				$( this ).find( 'input' ).attr( 'aria-required', 'true' );
+			});
+
+			if ( radio.length ) {
+
+				field.addClass( 'forminator-is_required' );
+				field.attr( 'aria-required', 'true' );
+
+				if ( label.length ) {
+					label.html( text + ' ' + required );
+				}
+			}
+		}
+
+		function init() {
+
+			if ( true === column.data( 'required' ) ) {
+				requiredInput( column );
+				requiredTextarea( column );
+				requiredRadio( column );
+			}
+		}
+
+		init();
+
+		return this;
+
+	};
+
 	$( 'body' ).ready( function() {
 
 		// Check if page exists
@@ -31,6 +129,9 @@
 							const select = column.find( '.forminator-select' );
 							const select2 = column.find( '.forminator-select2' );
 							const submit = column.find( '.forminator-button-submit' );
+
+							// Input required attributes
+							SHOWCASE.requiredSettings( column );
 
 							// Remove duplicated element
 							column.find( '> .forminator-col' ).unwrap();
