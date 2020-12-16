1fdbcc9e0fb5efa4dc9d64c31916c980546d34be
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2020-12-16 00:09:53 -0500

    [Fix] Adding a check if select2 is initialised.

--- library/js/select2.js
+++ library/js/select2.js
@@ -52,14 +52,18 @@
 						$language = 'en';
 					}
 
-					$select.FUIselect2({
-						dir: $dir,
-						language: $language,
-						placeholder: $placeholder,
-						dropdownCssClass: 'forminator-custom-form-' + $formid + ' forminator-dropdown--' + $theme
-					}).on( 'select2:opening', function() {
-						$select.data( 'select2' ).$dropdown.find( ':input.select2-search__field' ).attr( 'placeholder', $placeholder );
-					});
+					if ( 'function' === typeof $select.FUIselect2 ) {
+						$select.FUIselect2({
+							dir: $dir,
+							language: $language,
+							placeholder: $placeholder,
+							dropdownCssClass: 'forminator-custom-form-' + $formid + ' forminator-dropdown--' + $theme
+						}).on( 'select2:opening', function() {
+							$select.data( 'select2' ).$dropdown.find( ':input.select2-search__field' ).attr( 'placeholder', $placeholder );
+						});
+					} else {
+						$select.addClass( 'forminator-select' );
+					}
 				}
 			});
 		});
