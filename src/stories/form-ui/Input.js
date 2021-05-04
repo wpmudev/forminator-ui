4700c88c14950a1d5a4e7477d4c81a0d9dcc9068
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-05-04 18:32:51 -0500

    🐛 fix(input): Add more controls to component.

--- src/stories/form-ui/Input.js
+++ src/stories/form-ui/Input.js
@@ -2,24 +2,57 @@ export const createInput = ({
     design,
     label,
     description,
-    errorMessage
+    errorState,
+    errorMessage,
+    placeholder,
+    inputBorder,
+    inputBorderHover,
+    inputBackground,
+    inputBackgroundHover,
+    inputColor,
+    inputColorHover,
+    inputFontSize,
+    inputFontFamily,
+    inputFontWeight,
+    labelColor,
+    labelFontSize,
+    labelFontFamily,
+    labelFontWeight,
+    descColor,
+    descFontSize,
+    descFontWeight,
+    descFontFamily,
+    errorBackground,
+    errorColor,
+    errorFontSize,
+    errorFontWeight,
+    errorFontFamily
 }) => {
 
-    // Editable.
-    const styles = {};
-    styles.padding = 10;
-    styles.border = 1;
-    styles.borderRadius = 2;
+    // INPUT: Editable styles.
+    const inputCSS = {};
+    inputCSS.padding = 10;
+    inputCSS.borderWidth = 1;
+    inputCSS.borderRadius = 2;
+    inputCSS.borderColor = inputBorder;
+    inputCSS.borderColorHover = inputBorderHover;
+    inputCSS.background = inputBackground;
+    inputCSS.backgroundHover = inputBackgroundHover;
+    inputCSS.color = inputColor;
+    inputCSS.colorHover = inputColorHover;
+    inputCSS.fontSize = inputFontSize + 'px';
+    inputCSS.fontFamily = inputFontFamily;
+    inputCSS.fontWeight = inputFontWeight;
 
     switch (design) {
         case 'flat' :
-            styles.border = 0;
-            styles.borderRadius = 0;
+            inputCSS.borderWidth = 0;
+            inputCSS.borderRadius = 0;
             break;
 
         case 'bold' :
-            styles.border = 3;
-            styles.borderRadius = 0;
+            inputCSS.borderWidth = 3;
+            inputCSS.borderRadius = 0;
             break;
     }
 
@@ -30,33 +63,107 @@ export const createInput = ({
         case 'default':
         case 'flat':
         case 'bold':
-            input.style.padding = (styles.padding - styles.border) + 'px';
-            input.style.borderWidth = styles.border + 'px';
-            input.style.borderRadius = styles.borderRadius + 'px';
+            input.style.padding = (inputCSS.padding - inputCSS.borderWidth) + 'px';
+            input.style.borderWidth = inputCSS.borderWidth + 'px';
+            input.style.borderRadius = inputCSS.borderRadius + 'px';
+            input.style.borderColor = inputCSS.borderColor;
+            input.style.background = inputCSS.background;
+            input.style.color = inputCSS.color;
+            input.style.fontSize = inputCSS.fontSize;
+            if ( 'inherit' !== inputCSS.fontFamily ) {
+                input.style.fontFamily = inputCSS.fontFamily;
+            }
+            input.style.fontWeight = inputCSS.fontWeight;
             break;
     }
+
+    if ( '' !== placeholder || 'undefined' !== typeof placeholder ) {
+        input.setAttribute( 'placeholder', placeholder );
+    }
+
+    // LABEL: Editable styles.
+    const labelCSS = {};
+    labelCSS.color = labelColor;
+    labelCSS.fontSize = labelFontSize + 'px';
+    labelCSS.fontFamily = labelFontFamily;
+    labelCSS.fontWeight = labelFontWeight;
 
     // Create label.
     const inputLabelWrapper = document.createElement('label');
     const inputLabelContent = document.createTextNode(label);
     inputLabelWrapper.appendChild(inputLabelContent);
     inputLabelWrapper.classList = 'forminator-label';
+    switch (design) {
+        case 'default':
+        case 'flat':
+        case 'bold':
+            inputLabelWrapper.style.color = labelCSS.color;
+            inputLabelWrapper.style.fontSize = labelCSS.fontSize;
+            if ( 'inherit' !== labelCSS.fontFamily ) {
+                inputLabelWrapper.style.fontFamily = labelCSS.fontFamily;
+            }
+            inputLabelWrapper.style.fontWeight = labelCSS.fontWeight;
+            break;
+    }
+
+    // ERROR MESSAGE: Editable styles.
+    const errorCSS = {};
+    errorCSS.background = errorBackground;
+    errorCSS.color = errorColor;
+    errorCSS.fontSize = errorFontSize + 'px';
+    errorCSS.fontFamily = errorFontFamily;
+    errorCSS.fontWeight = errorFontWeight;
 
     // Create error message.
     const inputErrorWrapper = document.createElement('span');
     const inputErrorContent = document.createTextNode(errorMessage);
     inputErrorWrapper.appendChild(inputErrorContent);
     inputErrorWrapper.classList = 'forminator-error-message';
+    switch (design) {
+        case 'default':
+        case 'flat':
+        case 'bold':
+            inputErrorWrapper.style.background = errorCSS.background;
+            inputErrorWrapper.style.color = errorCSS.color;
+            inputErrorWrapper.style.fontSize = errorCSS.fontSize;
+            if ( 'inherit' !== errorCSS.fontFamily ) {
+                inputErrorWrapper.style.fontFamily = errorCSS.fontFamily;
+            }
+            inputErrorWrapper.style.fontWeight = errorCSS.fontWeight;
+            break;
+    }
+
+    // DESCRIPTION: Editable styles.
+    const descCSS = {};
+    descCSS.color = descColor;
+    descCSS.fontSize = descFontSize + 'px';
+    descCSS.fontFamily = descFontFamily;
+    descCSS.fontWeight = descFontWeight;
 
     // Create description.
     const inputDescWrapper = document.createElement('span');
     const inputDescContent = document.createTextNode(description);
     inputDescWrapper.appendChild(inputDescContent);
     inputDescWrapper.classList = 'forminator-description';
+    switch (design) {
+        case 'default':
+        case 'flat':
+        case 'bold':
+            inputDescWrapper.style.color = descCSS.color;
+            inputDescWrapper.style.fontSize = descCSS.fontSize;
+            if ( 'inherit' !== descCSS.fontFamily ) {
+                inputDescWrapper.style.fontFamily = descCSS.fontFamily;
+            }
+            inputDescWrapper.style.fontWeight = descCSS.fontWeight;
+            break;
+    }
 
     // Create field.
     const field = document.createElement('div');
     field.classList = 'forminator-field';
+    if ( errorState ) {
+        field.classList.add( 'forminator-has_error' );
+    }
     if ( 'undefined' !== typeof label && '' !== label ) {
         field.appendChild(inputLabelWrapper);
     }
