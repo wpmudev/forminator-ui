ae9bb4986463e8c5db3db5fe88bb747e75c41f0a
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-05-09 18:28:34 -0500

    🐛 fix(gulp): Improve tasks to run on packages.

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,38 @@
 'use strict';
 
+var path = require('path');
 var gulp = require('gulp');
+var del = require('del');
+var gutil = require('gulp-util');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
+var MonorepoTasks = require('gulp-tasks-monorepo');
 
 sass.compiler = require('node-sass');
 
-var paths = {
-    styles: {
-        source: 'packages/**/lib/scss/**/*.scss',
-        story: 'src/stories/assets/css/',
-        dest: 'packages/**/dist/css/'
-    }
-};
+var repo = MonorepoTasks({
+    dir: path.join( __dirname, '/packages' )
+});
 
-function clean() {
-    return del(['dist']);
-}
+repo.task( 'clean', function clean( pkg ) {
+    gutil.log( 'Cleaning', pkg.name(), 'package' );
+    return del([
+        path.join( pkg.location(), '/dist/css' ),
+        path.join( pkg.location(), '/docs/assets/css' )
+    ]);
+});
 
-function styles() {
-    return gulp.src(paths.styles.source)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(cleanCSS())
-        .pipe(rename({
+repo.task( 'styles', function styles( pkg ) {
+    gutil.log( 'Building', pkg.name(), 'package' );
+
+    return gulp
+        .src( path.join( pkg.location(), '/lib/scss/**/*.scss' ) )
+        .pipe( sass().on( 'error', sass.logError ) )
+        .pipe( cleanCSS() )
+        .pipe( rename({
             suffix: '.min'
-        }))
-        .pipe(gulp.dest(paths.styles.story))
-        .pipe(gulp.dest(paths.styles.dest));
-}
-
-exports.clean = clean;
-exports.styles = styles;
+        }) )
+        .pipe( gulp.dest( path.join( pkg.location(), '/dist/css' ) ) )
+        .pipe( gulp.dest( path.join( pkg.location(), '/docs/assets/css' ) ) );
+});
