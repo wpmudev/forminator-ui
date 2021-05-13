a95c4d89f46ba1e9735a1417206a19a3004a54b8
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-05-12 21:06:14 -0500

    🐛 fix(storybook): Load jQuery.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
 	stories: [
 		'../packages/**/*.stories.mdx',
@@ -6,5 +8,17 @@ module.exports = {
 	addons: [
 		'@storybook/addon-links',
 		'@storybook/addon-essentials'
-	]
+	],
+	webpackFinal: async ( config ) => {
+
+		config.plugins.push(
+			new webpack.ProvidePlugin({
+				$: 'jquery',
+				jQuery: 'jquery'
+			})
+		);
+
+		return config;
+
+	}
 };
